9700edcd0d995830e65fa3aff7014b681e6fc8e4
DavidColatti <davidcolatti@gmail.com>
2020-03-18 09:14:50 -0400

    4 Katas Completed

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -6,6 +6,30 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
+function reverse(str) {
+    let reversed = '';
+
+    for (let character of str) {
+        reversed = character + reversed;
+    }
+    return reversed;
+}
 
 module.exports = reverse;
+
+// Other Solutions:
+
+
+// function reverse(str) {
+//     let arr = str.split('');
+//     arr.reverse();
+//     return arr.join('');
+// }
+
+
+// function reverse(str) {
+//     return str
+//     .split('')
+//     .reverse()
+//     .join('');
+// }
